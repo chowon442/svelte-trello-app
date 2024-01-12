0761b0f4f1755689097b2bd7ddb6cd0fc9cddcae
chowon442 <chowon442@gmail.com>
2024-01-13 07:09:22 +0900

    add Header

---
+++ src/components/Header.svelte
@@ -0,0 +1,19 @@
+<header>
+    <img src="./images/trello-logo.svg" alt="Trello" class="logo" />
+</header>
+
+<style>
+    header {
+        height: 40px;
+        box-sizing: border-box;
+        background-color: rgba(0, 0, 0, 0.4);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    header img {
+        width: 80px;
+        opacity: 0.5;
+    }
+</style>
